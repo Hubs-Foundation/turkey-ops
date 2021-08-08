f30effe11b34dcc1461650da5da11d853d1be625
Geng Tan <geng.tan@limeade.com>
2021-08-08 13:43:19 -0400

    words

--- cf-templates/cf-push.sh
+++ cf-templates/cf-push.sh
@@ -1,1 +1,5 @@
-aws s3 sync --exclude "*" --include "*.yaml" . s3://turkeycfg/cf
+env="dev"
+! [ -z "$1" ] && env=$1
+s3path="s3://turkeycfg/cf/$env"
+echo "pushing $s3path"
+aws s3 sync --exclude "*" --include "*.yaml" . s3://turkeycfg/cf/$env
