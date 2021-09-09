a29115dcc08dc028841b060d0b766d2410cfd714
Geng Tan <geng.tan@limeade.com>
2021-09-09 10:56:15 -0400

    wip-k8s

--- orchestrator/handlers/TurkeyDeployK8s.go
+++ orchestrator/handlers/TurkeyDeployK8s.go
@@ -97,7 +97,7 @@ var TurkeyDeployK8s = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reque
 		// fmt.Println(k8sChartYaml)
 		// return
 		// //		</dryRun>
-		sess.PushMsg("&#129311; ... k8s.cfg.ServerName: " + cfg.ServerName)
+		sess.PushMsg("&#129311; ... k8s.cfg.ServerName: " + cfg.Host)
 
 		// //-----------------------------------test k8s config
 		// clientset, err := kubernetes.NewForConfig(cfg)
