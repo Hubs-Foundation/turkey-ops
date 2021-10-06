51f58c0913fdd9d2cd6d9f0a5a929fc935c37304
Geng Tan <geng.tan@limeade.com>
2021-10-06 12:25:06 -0400

    wip

--- k8s-charts/_renderedTurkey/templify.sh
+++ k8s-charts/_renderedTurkey/templify.sh
@@ -8,7 +8,7 @@ sed -i 's/dev0/{{.Subdomain}}/g' $yam
 sed -i 's/someString/{{.TurkeyId}}/g' $yam
 sed -i 's/myhubs.net/{{.Domain}}/g' $yam
 sed -i 's/gtan@mozilla.com/{{.UserEmail}}/g' $yam
-sed -i 's/ret_dev/{{.DBname}}/g' $yam
+sed -i 's/ret_dev0/{{.DBname}}/g' $yam
 sed -i 's#  PERMS_KEY: ----.*#  PERMS_KEY: {{.PermsKey}}#g' $yam
 sed -i 's#".*3RY0qLmdthY6Q0RZ4oyNQSL035BmYLNdleX1qVpG1zfQeLWf.*#"{{.JWK}}"#g' $yam
 
