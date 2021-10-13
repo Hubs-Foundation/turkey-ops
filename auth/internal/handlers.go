0454ebd17ee218a5a546e73aa974b9e8e9e34000
Robin K Wilson <robin.kath.wilson@gmail.com>
2021-10-13 12:05:38 -0700

    google needs https, fxa does not

--- auth/internal/handlers.go
+++ auth/internal/handlers.go
@@ -85,7 +85,13 @@ func authRedirect(w http.ResponseWriter, r *http.Request, providerName string) {
 		logger.Info("You are using \"secure\" cookies for a request that was not " + "received via https. You should either redirect to https or pass the " + "\"insecure-cookie\" config option to permit cookies via http.")
 	}
 
-	loginURL := provider.GetLoginURL("auth."+cfg.Domain, MakeState(r, provider, nonce))
+	// todo is there a better way to do this in go?
+	redirectURL := "auth." + cfg.Domain
+	if providerName == "google" {
+		redirectURL = "https://auth." + cfg.Domain
+	}
+
+	loginURL := provider.GetLoginURL(redirectURL, MakeState(r, provider, nonce))
 	logger.Debug(" ### loginURL: " + loginURL)
 	http.Redirect(w, r, loginURL, http.StatusTemporaryRedirect)
 
