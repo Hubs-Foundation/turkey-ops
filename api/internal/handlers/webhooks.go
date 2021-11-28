5cd6357a8b8b247d9164ac00f02f9c5160fba8ec
Geng Tan <geng.tan@limeade.com>
2021-11-28 12:02:21 -0500

    wip

---
+++ api/internal/handlers/webhooks.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"main/internal"
+	"net/http"
+)
+
+var dockerhub = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var v map[string]interface{}
+	err := decoder.Decode(&v)
+	if err != nil {
+		internal.GetLogger().Warn("bad r.Body")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	internal.GetLogger().Info("new dockerhub deployment")
+
+	fmt.Println(dumpHeader(r))
+	b, _ := json.MarshalIndent(v, "", "  ")
+	fmt.Println(string(b))
+
+	return
+})
