efa1fc438c01aaa37b4ffbfe4230b8c3dd723615
Geng Tan <geng.tan@limeade.com>
2021-11-30 10:16:56 -0500

    wip

--- api/internal/handlers/webhooks.go
+++ api/internal/handlers/webhooks.go
@@ -5,27 +5,44 @@ import (
 	"fmt"
 	"main/internal"
 	"net/http"
+	"strings"
 )
+
+type dockerhubWebhookJson struct {
+	Callback_url string
+	Push_data    dockerhubWebhookJson_push_data
+}
+
+type dockerhubWebhookJson_push_data struct {
+	Pusher string
+	Tag    string
+}
 
 var Dockerhub = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/webhooks/dockerhub" || r.Method != "POST" {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
-	fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@/webhooks/dockerhub")
+
 	decoder := json.NewDecoder(r.Body)
-	var v map[string]interface{}
+	var v dockerhubWebhookJson
 	err := decoder.Decode(&v)
-	if err != nil {
-		internal.GetLogger().Warn("bad r.Body")
+	if err != nil || !strings.HasPrefix(v.Callback_url, "https://registry.hub.docker.com/u/mozillareality/") {
+		internal.GetLogger().Warn("bad r.Body" + err.Error())
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
-	internal.GetLogger().Info("new dockerhub deployment")
 
-	fmt.Println(dumpHeader(r))
-	b, _ := json.MarshalIndent(v, "", "  ")
-	fmt.Println(string(b))
+	internal.GetLogger().Debug(fmt.Sprintf("%+v", v))
 
-	return
+	if strings.HasPrefix(v.Push_data.Tag, ".dev") ||
+		strings.HasPrefix(v.Push_data.Tag, ".staging") ||
+		strings.HasPrefix(v.Push_data.Tag, ".prod") {
+		internal.GetLogger().Info("deploying: " + v.Push_data.Tag)
+	}
+
+	// fmt.Println(dumpHeader(r))
+	// b, _ := json.MarshalIndent(v, "", "  ")
+	// fmt.Println(string(b))
+
 })
