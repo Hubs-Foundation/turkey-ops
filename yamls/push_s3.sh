918b4f6c7f918a6c42e8778caa3e55e2da9ec1f6
Geng Tan <geng.tan@limeade.com>
2022-01-08 15:31:10 -0500

    wip

--- yamls/push_s3.sh
+++ yamls/push_s3.sh
@@ -4,4 +4,4 @@
 
 #aws s3 sync --exclude "*" --include "*.yaml" ./cfs s3://$s3bkt/$env/cfs
 #aws s3 sync --exclude "*" --include "*.yam" ./yams s3://$s3bkt/$env/yams
-aws s3 sync . s3://$s3bkt/$env/cfs
+aws s3 sync . s3://$s3bkt/$env --exclude "*" --include "*.yaml" --include "*.yam"
