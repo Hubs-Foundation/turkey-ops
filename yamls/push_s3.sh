942755b668276af997d4a77334b5dc607c22a752
Geng Tan <geng.tan@limeade.com>
2022-01-21 08:40:13 -0500

    wip

--- yamls/push_s3.sh
+++ yamls/push_s3.sh
@@ -2,6 +2,7 @@
 [ -z "$s3bkt" ] && s3bkt="turkeycfg"
 [ -z "$env" ] && env="dev"
 
-#aws s3 sync --exclude "*" --include "*.yaml" ./cfs s3://$s3bkt/$env/cfs
-#aws s3 sync --exclude "*" --include "*.yam" ./yams s3://$s3bkt/$env/yams
-aws s3 sync . s3://$s3bkt/$env --exclude "*" --include "*.yaml" --include "*.yam"
+s3SyncTo="s3://$s3bkt/$env"
+echo "[info] pushing to: $s3SyncTo"
+
+aws s3 sync . $s3SyncTo --exclude "*" --include "*.yaml" --include "*.yam"
