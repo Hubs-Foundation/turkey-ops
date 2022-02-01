62dcb39e46e3d87e70c5662653e3101a3807755b
Geng Tan <geng.tan@limeade.com>
2022-02-01 13:46:18 -0500

    wip

--- deps/s3fs/entrypoint.sh
+++ deps/s3fs/entrypoint.sh
@@ -1,2 +1,4 @@
+
+s3fs --version
 echo "$AWS_KEY:$AWS_SECRET" >> s3fsPasswd && chmod 600 s3fsPasswd
-s3fs "$S3_BUCKET" "$MNT_DIR" -o passwd_file=s3fsPasswd  && tail -f /dev/null
+s3fs "$S3_BUCKET" "$MNT_DIR" -o passwd_file=s3fsPasswd #&& tail -f /dev/null
