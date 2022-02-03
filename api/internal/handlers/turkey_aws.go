2726f4510928052ded0598b58563453b41518f4c
Geng Tan <geng.tan@limeade.com>
2022-02-03 17:27:22 -0500

    wip

--- api/internal/handlers/turkey_aws.go
+++ api/internal/handlers/turkey_aws.go
@@ -41,10 +41,11 @@ type clusterCfg struct {
 	AWS_REGION string `json:"AWS_REGION"` //us-east-1
 
 	//optional inputs
-	DeploymentName  string `json:"name"`            //z
-	CF_deploymentId string `json:"cf_deploymentId"` //s0meid
+	CF_DeploymentPrefix string `json:"name"`            //t-
+	CF_deploymentId     string `json:"cf_deploymentId"` //s0meid
 
 	//generated pre-infra-deploy
+	CF_Stackname  string `json:"stackname"`
 	DB_PASS       string `json:"DB_PASS"`       //itjfHE8888
 	COOKIE_SECRET string `json:"COOKIE_SECRET"` //a-random-string-to-sign-auth-cookies
 	PERMS_KEY     string `json:"PERMS_KEY"`     //-----BEGIN RSA PRIVATE KEY-----\\nMIIEpgIBAAKCAQEA3RY0qLmdthY6Q0RZ4oyNQSL035BmYLNdleX1qVpG1zfQeLWf\\n/otgc8Ho2w8y5wW2W5vpI4a0aexNV2evgfsZKtx0q5WWwjsr2xy0Ak1zhWTgZD+F\\noHVGJ0xeFse2PnEhrtWalLacTza5RKEJskbNiTTu4fD+UfOCMctlwudNSs+AkmiP\\nSxc8nWrZ5BuvdnEXcJOuw0h4oyyUlkmj+Oa/ZQVH44lmPI9Ih0OakXWpIfOob3X0\\nXqcdywlMVI2hzBR3JNodRjyEz33p6E//lY4Iodw9NdcRpohGcxcgQ5vf4r4epLIa\\ncr0y5w1ZiRyf6BwyqJ6IBpA7yYpws3r9qxmAqwIDAQABAoIBAQCgwy/hbK9wo3MU\\nTNRrdzaTob6b/l1jfanUgRYEYl/WyYAu9ir0JhcptVwERmYGNVIoBRQfQClaSHjo\\n0L1/b74aO5oe1rR8Yhh+yL1gWz9gRT0hyEr7paswkkhsmiY7+3m5rxsrfinlM+6+\\nJ7dsSi3U0ofOBbZ4kvAeEz/Y3OaIOUbQraP312hQnTVQ3kp7HNi9GcLK9rq2mASu\\nO0DxDHXdZMsRN1K4tOKRZDsKGAEfL2jKN7+ndvsDhb4mAQaVKM8iw+g5O4HDA8uB\\nmwycaWhjilZWEyUyqvXE8tOMLS59sq6i1qrf8zIMWDOizebF/wnrQ42kzt5kQ0ZJ\\nwCPOC3sxAoGBAO6KfWr6WsXD6phnjVXXi+1j3azRKJGQorwQ6K3bXmISdlahngas\\nmBGBmI7jYTrPPeXAHUbARo/zLcbuGCf1sPipkAHYVC8f9aUbA205BREB15jNyXr3\\nXzhR/ronbn0VeR9iRua2FZjVChz22fdz9MvRJiinP8agYIQ4LovDk3lzAoGBAO1E\\nrZpOuv3TMQffPaPemWuvMYfZLgx2/AklgYqSoi683vid9HEEAdVzNWMRrOg0w5EH\\nWMEMPwJTYvy3xIgcFmezk5RMHTX2J32JzDJ8Y/uGf1wMrdkt3LkPRfuGepEDDtBa\\nrUSO/MeGXLu5p8QByUZkvTLJ4rJwF2HZBUehrm3pAoGBANg1+tveNCyRGbAuG/M0\\nvgXbwO+FXWojWP1xrhT3gyMNbOm079FI20Ty3F6XRmfRtF7stRyN5udPGaz33jlJ\\n/rBEsNybQiK8qyCNzZtQVYFG1C4SSI8GbO5Vk7cTSphhwDlsEKvJWuX+I36BWKts\\nFPQwjI/ImIvmjdUKP1Y7XQ51AoGBALWa5Y3ASRvStCqkUlfFH4TuuWiTcM2VnN+b\\nV4WrKnu/kKKWs+x09rpbzjcf5kptaGrvRp2sM+Yh0RhByCmt5fBF4OWXRJxy5lMO\\nT78supJgpcbc5YvfsJvs9tHIYrPvtT0AyrI5B33od74wIhrCiz5YCQCAygVuCleY\\ndpQXSp1RAoGBAKjasot7y/ErVxq7LIpGgoH+XTxjvMsj1JwlMeK0g3sjnun4g4oI\\nPBtpER9QaSFi2OeYPklJ2g2yvFcVzj/pFk/n1Zd9pWnbU+JIXBYaHTjmktLeZHsb\\nrTEKATo+Y1Alrhpr/z7gXXDfuKKXHkVRiper1YRAxELoLJB8r7LWeuIb\\n-----END RSA PRIVATE KEY-----
@@ -99,21 +100,20 @@ var TurkeyAws = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			{Key: aws.String("turkeyDomain"), Value: aws.String(cfg.Domain)},
 		}
 		// ######################################### 3. run cloudformation #########################################
-		stackName := cfg.DeploymentName + "-" + cfg.CF_deploymentId
 		go func() {
-			err = awss.CreateCFstack(stackName, cfS3Folder+"main.yaml", cfParams, cfTags)
+			err = awss.CreateCFstack(cfg.CF_Stackname, cfS3Folder+"main.yaml", cfParams, cfTags)
 			if err != nil {
-				sess.Panic("ERROR @ CreateCFstack for " + stackName + ": " + err.Error())
+				sess.Panic("ERROR @ CreateCFstack for " + cfg.CF_Stackname + ": " + err.Error())
 				return
 			}
 
 		}()
-		sess.Log("&#128640;CreateCFstack started for stackName=" + stackName)
-		reportCreateCFstackStatus(stackName, cfg, sess, awss)
+		sess.Log("&#128640;CreateCFstack started for stackName=" + cfg.CF_Stackname)
+		reportCreateCFstackStatus(cfg.CF_Stackname, cfg, sess, awss)
 		// ######################################### 4. post deployment configs ###################################
-		report, err := postDeploymentConfigs(cfg, stackName, awss, r.Header.Get("X-Forwarded-UserEmail"), sess)
+		report, err := postDeploymentConfigs(cfg, cfg.CF_Stackname, awss, r.Header.Get("X-Forwarded-UserEmail"), sess)
 		if err != nil {
-			sess.Panic("ERROR @ postDeploymentConfigs for " + stackName + ": " + err.Error())
+			sess.Panic("ERROR @ postDeploymentConfigs for " + cfg.CF_Stackname + ": " + err.Error())
 		}
 		sess.Log(" --- report for (" + cfg.CF_deploymentId + ") --- sknoonerToken: " + report["skoonerToken"])
 		sess.Log(" --- report for (" + cfg.CF_deploymentId + ") --- you must create this CNAME record manually in your nameserver {" +
@@ -266,14 +266,15 @@ func turkey_makeCfg(r *http.Request, sess *internal.CacheBoxSessData) (clusterCf
 	}
 
 	//optional inputs
-	if cfg.DeploymentName == "" {
-		cfg.DeploymentName = "t"
-		internal.GetLogger().Warn("DeploymentName unspecified -- using (default)" + cfg.DeploymentName)
+	if cfg.CF_DeploymentPrefix == "" {
+		cfg.CF_DeploymentPrefix = "t-"
+		internal.GetLogger().Warn("CF_DeploymentPrefix unspecified -- using (default)" + cfg.CF_DeploymentPrefix)
 	}
 	if cfg.CF_deploymentId == "" {
 		cfg.CF_deploymentId = strconv.FormatInt(time.Now().Unix()-1626102245, 36)
 		internal.GetLogger().Info("CF_deploymentId: " + cfg.CF_deploymentId)
 	}
+	cfg.CF_Stackname = cfg.CF_DeploymentPrefix + cfg.CF_deploymentId
 
 	//generate the rest
 	cfg.DB_PASS = internal.PwdGen(15)
