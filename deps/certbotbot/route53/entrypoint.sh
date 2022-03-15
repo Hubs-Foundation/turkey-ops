fb7a38d228cfbbf7cd4ac30e3146f78c779280eb
Geng Tan <geng.tan@limeade.com>
2022-03-15 14:53:06 -0400

    wip

--- deps/certbotbot/route53/entrypoint.sh
+++ deps/certbotbot/route53/entrypoint.sh
@@ -36,7 +36,7 @@ get_kubectl
 if ! need_new_cert; then echo "good cert, exit in 15 min"; sleep 900; exit 0; fi
 echo "getting new cert"
 if ! get_new_cert; then echo "ERROR failed to get new cert, exit in 15 min"; sleep 900; exit 1; fi
-if ! save_cert; then echo "ERROR failed to save cert"; fi
+if ! save_cert; then echo "ERROR failed to save cert"; sleep 900; fi
 
 
 
