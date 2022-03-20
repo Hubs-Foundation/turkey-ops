e2ae2b8a252a5485faa9c179ee26fef066607268
Geng Tan <geng.tan@limeade.com>
2022-03-20 00:39:38 -0400

    wip

--- deps/gcsfuse/entrypoint.sh
+++ deps/gcsfuse/entrypoint.sh
@@ -4,4 +4,4 @@ gcsfuse --version
 keyfile="/gcsfuse/gcp_sa_key.json"
 echo "$GCP_SA_KEY" > $keyfile && chmod 600 $keyfile
 export GOOGLE_APPLICATION_CREDENTIALS=$keyfile
-gcsfuse "$GCS_BUCKET" "$MNT_DIR"
+gcsfuse "$GCS_BUCKET" "$MNT_DIR" -o allow_other --foreground --implicit-dirs
