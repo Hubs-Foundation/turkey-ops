c8a64c1659b66f475ed4b95844ec9fc77840b741
Geng Tan <geng.tan@limeade.com>
2022-03-22 00:34:41 -0400

    wip

--- deps/goofys/entrypoint.sh
+++ deps/goofys/entrypoint.sh
@@ -1,8 +1,10 @@
 
 mkdir -p $MNT_DIR
+export cacheDir="/home/goofys-cache"
+mkdir -p 
 ./goofys --version
 keyfile="/gcsfuse/gcp_sa_key.json"
 echo "$GCP_SA_KEY" > $keyfile && chmod 600 $keyfile
 export GOOGLE_APPLICATION_CREDENTIALS=$keyfile
-./goofys -f --cache /home/goofys-cache -o allow_other "gs://$GCS_BUCKET" "$MNT_DIR" 
+./goofys -f --cache $cacheDir -o allow_other "gs://$GCS_BUCKET" "$MNT_DIR" 
 
