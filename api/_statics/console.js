14b18fb840cb5792ec0160b532ca2915b98a975c
Geng Tan <geng.tan@limeade.com>
2022-03-28 00:56:34 -0400

    wip

--- api/_statics/console.js
+++ api/_statics/console.js
@@ -56,7 +56,7 @@ function orcReq(method, path) {
   cfg=document.getElementById("cfg").value
   var xhttp = new XMLHttpRequest(); res=""
   xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {res = this.responseText;}};
-  xhttp.open("POST", "/hc_instance", true);
+  xhttp.open(method, path, true);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(cfg);
 }
