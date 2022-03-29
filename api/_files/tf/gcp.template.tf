38668341954e4083248d55c657ec25008d2f4d43
Geng Tan <geng.tan@limeade.com>
2022-03-29 10:34:34 -0400

    wip

--- api/_files/tf/gcp.template.tf
+++ api/_files/tf/gcp.template.tf
@@ -148,7 +148,7 @@ resource "google_sql_database_instance" "pgsql" {
   region           = "{{.Region}}"
   deletion_protection = false
   settings {
-    tier = "db-f1-micro"
+    tier = "db-custom-1-8192"
     ip_configuration {
       ipv4_enabled    = true
       private_network = google_compute_network.vpc.id
