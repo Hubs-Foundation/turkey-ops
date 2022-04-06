9aaac8cb9284b15fb12879459eb92a9b09759671
Geng Tan <geng.tan@limeade.com>
2022-04-06 13:18:52 -0400

    wip

--- auth/main.go
+++ auth/main.go
@@ -30,19 +30,31 @@ func main() {
 
 	router.Handle("/chk_cookie", internal.ChkCookie())
 
-	router.Handle("/gimmie_test_jwt_cookie", internal.GimmieTestJwtCookie())
+	router.Handle("/gimmie_test_jwt_cookie", internalEndpoint()(internal.GimmieTestJwtCookie()))
 
-	router.Handle("/zaplvl", privateEndpoint("dev")(internal.Atom))
+	router.Handle("/zaplvl", internalEndpoint()(internal.Atom))
 	//curl localhost:9001/zaplvl
 	//apk add curl; curl -X PUT -d 'level=debug' localhost:9001/zaplvl
 
 	internal.StartServer(router, 9001)
 }
 
-func privateEndpoint(requiredRole string) func(http.Handler) http.Handler {
+// func privateEndpoint(requiredRole string) func(http.Handler) http.Handler {
+// 	return func(next http.Handler) http.Handler {
+// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 			fmt.Println("~~~~~~~~~~~privateEndpoint~~~~~~~~~~~")
+// 			next.ServeHTTP(w, r)
+// 		})
+// 	}
+// }
+func internalEndpoint() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("~~~~~~~~~~~privateEndpoint~~~~~~~~~~~")
+			if r.Header.Get("X-Forwarded-For") != "" {
+				http.NotFound(w, r)
+				return
+			}
+			fmt.Println("~~~~~~~~~~~internalEndpoint~~~~~~~~~~~")
 			next.ServeHTTP(w, r)
 		})
 	}
