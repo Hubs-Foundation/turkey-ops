0a6c0a7d4bac47b4c7c6b9bdb1e17c6875a65ae0
Geng Tan <geng.tan@limeade.com>
2022-04-12 11:26:42 -0400

    wip

--- deps/ytdl/api_flask/main.py
+++ deps/ytdl/api_flask/main.py
@@ -179,8 +179,8 @@ def ytdl_api_stats():
     stats=redis_client.zrangebyscore(rkey, 0, 9999, withscores=True)
 
     if len(stats)>0:
-        report["_top_ip"] = str(stats[0][0])
-        report["_top_cnt"] = str(stats[0][1])
+        report["_top_ip"] = str(stats[len(stats)-1][0])
+        report["_top_cnt"] = str(stats[len(stats)-1][1])
         for ip, cnt in stats:
             report[str(cnt)]=str(ip)
     
