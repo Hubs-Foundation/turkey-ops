e2611fac3df571e44c054115796a8901d90669dc
Geng Tan <geng.tan@limeade.com>
2022-04-19 22:49:15 -0400

    wip

--- api/internal/handlers/hc_instance.go
+++ api/internal/handlers/hc_instance.go
@@ -118,17 +118,22 @@ func hc_create(w http.ResponseWriter, r *http.Request) {
 	k8sChartYaml := renderedYamls[0]
 
 	// #3 pre-deployment checks
+	result := "created."
 	// is subdomain available?
 	nsList, _ := internal.Cfg.K8ss_local.ClientSet.CoreV1().Namespaces().List(context.Background(),
 		metav1.ListOptions{LabelSelector: "Subdomain=" + hcCfg.Subdomain})
 	if len(nsList.Items) != 0 {
-		internal.Logger.Error("error @ k8s pre-deployment checks: subdomain already exists: " + hcCfg.Subdomain)
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"result": "subdomain already exists",
-			"hub_id": hcCfg.HubId,
-		})
-		return
+		if strings.HasSuffix(r.Header.Get("X-Forwarded-UserEmail"), "@mozilla.com") {
+			result += "subdomain overridden for @mozilla.com user"
+		} else {
+			internal.Logger.Error("error @ k8s pre-deployment checks: subdomain already exists: " + hcCfg.Subdomain)
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"result": "subdomain already exists",
+				"hub_id": hcCfg.HubId,
+			})
+			return
+		}
 	}
 
 	// #4 kubectl apply -f <file.yaml> --server-side --field-manager "turkey-userid-<cfg.UserId>"
@@ -168,7 +173,7 @@ func hc_create(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]interface{}{
-		"result":        "created",
+		"result":        result,
 		"useremail":     hcCfg.UserEmail,
 		"hub_id":        hcCfg.HubId,
 		"subdomain":     hcCfg.Subdomain,
