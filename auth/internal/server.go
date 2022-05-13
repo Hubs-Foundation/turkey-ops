4fbf7fa902b28c360e09d55700c2ded6307a8827
Geng Tan <geng.tan@limeade.com>
2022-05-13 00:31:02 -0400

    wip

--- auth/internal/server.go
+++ auth/internal/server.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"net/http"
@@ -37,6 +38,17 @@ func StartServer(router *http.ServeMux, port int) {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  15 * time.Second,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			},
+		},
 	}
 
 	done := make(chan bool)
@@ -59,7 +71,8 @@ func StartServer(router *http.ServeMux, port int) {
 	}()
 	Logger.Info("Server is ready to handle requests at" + listenAddr)
 	atomic.StoreInt32(&Healthy, 1)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+
+	if err := server.ListenAndServeTLS("cert.pem", "key.pem"); err != nil && err != http.ErrServerClosed {
 		Logger.Sugar().Fatalf("Could not listen on %s: %v\n", listenAddr, err)
 	}
 	<-done
