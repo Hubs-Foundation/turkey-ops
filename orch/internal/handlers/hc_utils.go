f2d10ed24c9da807fc791dc11f271594e3fe9527
Geng Tan <geng.tan@limeade.com>
2022-06-14 08:46:54 -0400

    words

--- orch/internal/handlers/hc_utils.go
+++ orch/internal/handlers/hc_utils.go
@@ -107,7 +107,7 @@ var TurkeyReturnCenter = http.HandlerFunc(func(w http.ResponseWriter, r *http.Re
 	if waitReq > 0 {
 		internal.Logger.Debug("on coolDown bounc for: " + nsName)
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprint(w, fmt.Sprintf(`%v <br> -->  <a href=https://"%v">%v</a> (try again in %v)`, trc_cd_RespMsg, goods, goods, waitReq.String()))
+		fmt.Fprint(w, fmt.Sprintf(`%v <br> -->  <a href=https://%v>%v</a> (try again in %v)`, trc_cd_RespMsg, goods, goods, waitReq.String()))
 		return
 	}
 
