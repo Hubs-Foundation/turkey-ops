50e2f699e6f7db87e8998b5f5e70c6703789d957
Geng Tan <geng.tan@limeade.com>
2022-06-15 16:36:42 -0400

    words

--- deps/goofys/entrypoint.sh
+++ deps/goofys/entrypoint.sh
@@ -1,7 +1,7 @@
 
 ./goofys --version
 
-keyfile="/gcsfuse/gcp_sa_key.json"
+keyfile="/goofys/gcp_sa_key.json"
 echo "$GCP_SA_KEY" > $keyfile && chmod 600 $keyfile
 export GOOGLE_APPLICATION_CREDENTIALS=$keyfile
-gcsfuse "gs://$GCS_BUCKET/$GCS_DIR" "$MNT_DIR"
+./goofys "gs://$GCS_BUCKET/$GCS_DIR" "$MNT_DIR"
