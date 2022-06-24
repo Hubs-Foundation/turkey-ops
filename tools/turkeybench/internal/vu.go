704d873d21efb196b71e7a8fd703ce8e85d883df
Geng Tan <geng.tan@limeade.com>
2022-06-24 12:10:52 -0400

    words

--- tools/turkeybench/internal/vu.go
+++ tools/turkeybench/internal/vu.go
@@ -11,36 +11,44 @@ import (
 	"time"
 )
 
-var (
-	turkeyDomain      = "gtan.myhubs.net"
-	_turkeyauthcookie = "WmeluLWXgwZ_U3Vk6k073-dtT9jNCd1WJ6_7vRkhKh4=|1655339779|gtan@mozilla.com"
-
-	useremail = "gtan@mozilla.com"
-
-	_httpClient = &http.Client{
-		Timeout:   10 * time.Second,
-		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
-	}
-)
+var _httpClient = &http.Client{
+	Timeout:   100 * time.Second,
+	Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+}
 
 type Vuser struct {
+	turkeyDomain      string //	ie. "gtan.myhubs.net"
+	_turkeyauthcookie string // ie. "WmeluLWXgwZ_U3Vk6k073-dtT9jNCd1WJ6_7vRkhKh4=|1655339779|gtan@mozilla.com"
+	useremail         string // 	ie. "gtan@mozilla.com"
+
+	Id      string
 	HubId   string
 	TCreate time.Duration
 	TReady  time.Duration
+	Url     string
+}
+
+func NewVuser(Id, domain, authcookie, email, hubId string) *Vuser {
+	return &Vuser{
+		turkeyDomain:      domain,
+		_turkeyauthcookie: authcookie,
+		useremail:         email,
+		HubId:             hubId,
+		Url:               "https://" + hubId + "." + domain + "/?skipadmin",
+		Id:                Id,
+	}
 }
 
 func (vu *Vuser) Create() {
-	fmt.Printf("\n######creating######, vu: %v", vu.HubId)
-
 	//create
 	createReqBody, _ := json.Marshal(map[string]string{
 		"hub_id":    vu.HubId,
-		"useremail": useremail,
+		"useremail": vu.useremail,
 	})
-	createReq, err := http.NewRequest("POST", "https://orch."+turkeyDomain+"/hc_instance", bytes.NewBuffer(createReqBody))
+	createReq, err := http.NewRequest("POST", "https://orch."+vu.turkeyDomain+"/hc_instance", bytes.NewBuffer(createReqBody))
 	createReq.AddCookie(&http.Cookie{
 		Name:  "_turkeyauthcookie",
-		Value: _turkeyauthcookie,
+		Value: vu._turkeyauthcookie,
 	})
 	if err != nil {
 		panic("createReq error: " + err.Error())
@@ -48,32 +56,32 @@ func (vu *Vuser) Create() {
 
 	tStart := time.Now()
 	resp, err := _httpClient.Do(createReq)
+	if err != nil {
+		panic(err)
+	}
 	bodyBytes, _ := io.ReadAll(resp.Body)
 	if err != nil && resp.StatusCode != http.StatusOK && strings.Contains(string(bodyBytes), `"result":"done"`) {
 		fmt.Printf("err: %v, resp: %v, resp.body: %v", err, resp, string(bodyBytes))
 		panic("failed @ creation")
 	}
-	fmt.Printf("\n---creation succeeded")
-
 	vu.TCreate = time.Since(tStart)
-	fmt.Printf("\n[tCreate: %v] for hubId %v", vu.TCreate, vu.HubId)
-
+	fmt.Printf("\n[%v] -- [tCreate: %v]", vu.Id, vu.TCreate)
 	//wait for ret
 	tStart = time.Now()
-	timeout := time.Now().Add(5 * time.Minute)
-	retReq, _ := http.NewRequest("GET", "https://"+vu.HubId+"."+turkeyDomain+"/?skipadmin", nil)
-	fmt.Printf("\nreq: %v", retReq.URL)
+	timeout := time.Now().Add(15 * time.Minute)
+	retReq, _ := http.NewRequest("GET", "https://"+vu.HubId+"."+vu.turkeyDomain+"/?skipadmin", nil)
+	// fmt.Printf("\nreq: %v", retReq.URL)
 	resp, err = _httpClient.Do(retReq)
 	for err != nil || resp.StatusCode != http.StatusOK {
 		time.Sleep(5 * time.Second)
 		ttl := time.Until(timeout)
-		if resp != nil {
-			// bodyBytes, _ := io.ReadAll(resp.Body)
-			// fmt.Printf("\n---[waiting-for-ret] got: %v[%v], retrying, ttl: %v, hubId: %v", resp.StatusCode, string(bodyBytes), ttl, vu.HubId)
-			fmt.Printf("\n---[waiting-for-ret] got: %v, retrying, ttl: %v, hubId: %v", resp.StatusCode, ttl, vu.HubId)
-		} else {
-			fmt.Printf("\n---[waiting-for-ret]: %v, retrying, ttl: %v, hubId: %v", err, ttl, vu.HubId)
-		}
+		// if resp != nil {
+		// 	// bodyBytes, _ := io.ReadAll(resp.Body)
+		// 	// fmt.Printf("\n---[waiting-for-ret] got: %v[%v], retrying, ttl: %v, hubId: %v", resp.StatusCode, string(bodyBytes), ttl, vu.HubId)
+		// 	fmt.Printf("\n---[waiting-for-ret] got: %v, retrying, ttl: %v, hubId: %v", resp.StatusCode, ttl, vu.HubId)
+		// } else {
+		// 	fmt.Printf("\n---[waiting-for-ret]: %v, retrying, ttl: %v, hubId: %v", err, ttl, vu.HubId)
+		// }
 		if ttl < 0 {
 			fmt.Printf("\n---[waiting-for-ret]:err: timeout -- hubId %v", vu.HubId)
 			vu.TReady = -1
@@ -83,19 +91,19 @@ func (vu *Vuser) Create() {
 	if vu.TReady != -1 {
 		vu.TReady = time.Since(tStart)
 	}
-	fmt.Printf("[tReady: %v] for hubId %v", vu.TCreate, vu.HubId)
+	fmt.Printf("\n [%v] [tReady: %v] @ %v", vu.Id, vu.TReady, vu.Url)
 }
 
 func (vu *Vuser) Delete() {
-	fmt.Printf("\n######deleting######, vu: %v", vu.HubId)
+	fmt.Printf("\n[deleting: %v]", vu.HubId)
 	//delete
 	deleteReqBody, _ := json.Marshal(map[string]string{
 		"hub_id": vu.HubId,
 	})
-	deleteReq, err := http.NewRequest("DELETE", "https://orch."+turkeyDomain+"/hc_instance", bytes.NewBuffer(deleteReqBody))
+	deleteReq, err := http.NewRequest("DELETE", "https://orch."+vu.turkeyDomain+"/hc_instance", bytes.NewBuffer(deleteReqBody))
 	deleteReq.AddCookie(&http.Cookie{
 		Name:  "_turkeyauthcookie",
-		Value: _turkeyauthcookie,
+		Value: vu._turkeyauthcookie,
 	})
 	if err != nil {
 		panic("deleteReq error: " + err.Error())
@@ -105,18 +113,20 @@ func (vu *Vuser) Delete() {
 		fmt.Printf("\nerr: %v, resp: %v", err, resp)
 		panic("failed @ delete")
 	}
-	fmt.Printf("\n[deleted: %v]", vu.HubId)
+	fmt.Printf("\n[%v] deleted", vu.HubId)
 }
 
-func (vu *Vuser) Load() {
-	fmt.Printf("\n######loading <fake ... real one with bots coming soon> ######, vu: %v", vu.HubId)
-	for i := 1; i <= 10; i++ {
-		time.Sleep(1 * time.Second)
-		fmt.Printf(". ")
+func (vu *Vuser) Load(ttl time.Duration) {
+	fmt.Printf("\n[%v] loading (just time.Sleep for now...)", vu.Id)
+	wait := 1 * time.Minute
+	for ttl > 0 {
+		time.Sleep(wait)
+		fmt.Printf("\n[%v] running, ttl: %v, url: %v", vu.Id, ttl, vu.Url)
+		ttl -= wait
 	}
-	fmt.Printf("done")
+	fmt.Printf("\n[%v] done", vu.Id)
 }
 
 func (vu *Vuser) ToString() string {
-	return fmt.Sprintf(`{ vu.HubId: %v, vu.tCreate: %v, vu.tReady: %v}`, vu.HubId, vu.TCreate, vu.TReady)
+	return fmt.Sprintf(`\n{ vu.Id: %v, vu.HubId: %v, vu.tCreate: %v, vu.tReady: %v}`, vu.Id, vu.HubId, vu.TCreate, vu.TReady)
 }
--- tools/turkeybench/main.go
+++ tools/turkeybench/main.go
@@ -2,20 +2,56 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	"main/internal"
 
 	"github.com/google/uuid"
 )
 
-func main() {
+var (
+	turkeyDomain      = "dev0.myhubs.net"
+	_turkeyauthcookie = "0BMxAoZ4TF7RQ9TJMecK2nwLRVOD-8xUptC-sYk0PFg=|1656121438|gtan@mozilla.com"
+	useremail         = "gtan@mozilla.com"
+	stepWait          = 250 * time.Millisecond
+)
 
-	vu := &internal.Vuser{
-		HubId: "tb" + strings.ReplaceAll(uuid.New().String(), "-", ""),
+// kubectl get ns | grep turkeybench | awk '{print $1}' | xargs kubectl delete ns
+func main() {
+	vuBag := []*internal.Vuser{}
+	for i := 0; i < 25; i++ {
+		vuBag = append(vuBag, internal.NewVuser(strconv.Itoa(i),
+			turkeyDomain, _turkeyauthcookie, useremail,
+			"turkeybench"+strings.ReplaceAll(uuid.New().String(), "-", ""),
+		))
 	}
-	vu.Create()
-	vu.Load()
-	vu.Delete()
-	fmt.Printf("\n###### %v", vu.ToString())
+	var wg_create sync.WaitGroup
+	for _, vu := range vuBag {
+		wg_create.Add(1)
+		go func(vu *internal.Vuser) {
+			defer wg_create.Done()
+			vu.Create()
+		}(vu)
+		time.Sleep(stepWait)
+	}
+	wg_create.Wait()
+
+	var wg_load sync.WaitGroup
+	for _, vu := range vuBag {
+		wg_load.Add(1)
+		go func(vu *internal.Vuser) {
+			defer wg_load.Done()
+			vu.Load(5 * time.Minute)
+		}(vu)
+	}
+	wg_load.Wait()
+
+	fmt.Println("=========================================================================")
+	for _, vu := range vuBag {
+		fmt.Println(vu.ToString())
+	}
+
 }
