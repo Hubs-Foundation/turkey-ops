b471d09d6a3b4be715dd0ef2d4726123e336e735
Geng Tan <geng.tan@limeade.com>
2022-07-04 12:43:38 -0400

    word

--- orch/_statics/console.js
+++ orch/_statics/console.js
@@ -84,9 +84,8 @@ function turkeyGcp_update(){
   var xhr = new XMLHttpRequest(); res=""
   xhr.onreadystatechange = function() {if (this.readyState == 4) {
     resJson = JSON.parse(this.responseText);
-    console.log("this.responseText: ", this.responseText)
-    console.log("resJson.tf_plan: ", resJson.tf_plan)
-    mbody.innerHTML=`<h1>${resJson.stackname}</h1><br><h2>${resJson.msg}</h2><br>`+resJson.tf_plan_html;
+    console.log(resJson.stackName, resJson.msg, resJson.tf_plan_html)
+    mbody.innerHTML=`<h1>${resJson.stackName}</h1><br><h2>${resJson.msg}</h2><br>`+resJson.tf_plan_html;
   }};
   xhr.open("PATCH", "/tco_gcp", false);
   // xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
