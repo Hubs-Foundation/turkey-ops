42df76cf70d2481c225f75a1b82665f805127e8c
Geng Tan <geng.tan@limeade.com>
2022-07-04 18:04:58 -0400

    word

--- orch/internal/handlers/turkey_gcp.go
+++ orch/internal/handlers/turkey_gcp.go
@@ -208,7 +208,7 @@ func tco_gcp_update(w http.ResponseWriter, r *http.Request) {
 		internal.Logger.Debug("[plan] [" + cfg.Stackname + "] completed")
 
 		for i := 0; i < len(tfout); i++ {
-			tfout[i] = string(ansihtml.ConvertToHTML([]byte(tfout[i])))
+			tfout[i] = string(ansihtml.ConvertToHTML([]byte("|" + tfout[i])))
 		}
 		tf_out_html := strings.Join(tfout, "<br>")
 
