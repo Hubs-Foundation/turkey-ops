28c0e14e5b0aac2fa00534d0f3eb41e3f7f074fc
Geng Tan <geng.tan@limeade.com>
2022-07-18 09:30:55 -0400

    words

--- orch/internal/handlers/utils.go
+++ orch/internal/handlers/utils.go
@@ -151,7 +151,9 @@ func turkey_loadStackCfg(stackname string, inputedCfg clusterCfg) (clusterCfg, e
 	//get cfg.json from turkeycfg bucket
 	cfgBytes, err := internal.Cfg.Gcps.GCS_ReadFile("turkeycfg", "tf-backend/"+stackname+"/cfg.json")
 	if err != nil {
-		return inputedCfg, err
+		// return inputedCfg, err
+		internal.Logger.Warn("failed to get cfg.json for " + stackname + ", returning inputedCfg unchanged")
+		return currentCfg, nil
 	}
 	//unmarshal to cfg
 	err = json.Unmarshal(cfgBytes, &currentCfg)
