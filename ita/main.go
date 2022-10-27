fb57a9f3e9efcd26649769bcbbf9bf162d5e2220
tanfarming <tanfarming@hotmail.com>
2022-10-27 00:27:45 -0400

    word

--- ita/main.go
+++ ita/main.go
@@ -21,11 +21,11 @@ func main() {
 	cron_1m := internal.NewCron("cron_1m", 1*time.Minute)
 	cron_15m := internal.NewCron("cron_15m", 15*time.Minute)
 	if strings.HasPrefix(internal.GetCfg().PodNS, "hc-") {
-		cron_1m.Load("pauseJob", internal.Cronjob_pauseHC)
+		// cron_1m.Load("pauseJob", internal.Cronjob_pauseHC)
+		cron_1m.Load("HcHealthchecks", internal.Cronjob_HcHealthchecks)
 	}
 	if internal.GetCfg().PodNS == "turkey-services" {
 		cron_1m.Load("turkeyBuildPublisher", internal.Cronjob_publishTurkeyBuildReport)
-		cron_1m.Load("HcHealthchecks", internal.Cronjob_HcHealthchecks)
 		cron_1m.Start()
 		cron_15m.Load("cleanupFailedPods", internal.Cronjob_cleanupFailedPods)
 		cron_15m.Load("SurveyStreamNodes", internal.Cronjob_SurveyStreamNodes)
