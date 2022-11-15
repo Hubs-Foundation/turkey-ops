80a666026ab4da7170b6ccd8d7eadf2b4afd5ea8
tanfarming <tanfarming@hotmail.com>
2022-11-15 14:25:45 -0500

    word

--- orch/internal/handlers/hc_instance.go
+++ orch/internal/handlers/hc_instance.go
@@ -746,7 +746,7 @@ func hc_patch_subdomain(HubId, Subdomain string) error {
 	for i, rule := range ingress.Spec.Rules {
 		hostArr := strings.SplitN(rule.Host, ".", 2)
 		newHost := strings.Replace(hostArr[0], oldSubdomain, Subdomain, 1) + "." + hostArr[1]
-		internal.Logger.Debug("newHost: " + newHost)
+		internal.Logger.Sugar().Debugf("hostArr[0]: %v, oldSubdomain: %v, Subdomain: %v, newHost: %v", hostArr[0], oldSubdomain, Subdomain, newHost)
 		ingress.Spec.Rules[i].Host = newHost
 	}
 	ingress.Annotations["haproxy.org/response-set-header"] = strings.Replace(
