2e9cf4e3129f74134a0df67617db041e791e9190
tanfarming <tanfarming@hotmail.com>
2022-11-15 15:42:50 -0500

    word

--- orch/internal/handlers/hc_instance.go
+++ orch/internal/handlers/hc_instance.go
@@ -745,12 +745,12 @@ func hc_patch_subdomain(HubId, Subdomain string) error {
 	}
 	for i, rule := range ingress.Spec.Rules {
 		hostArr := strings.SplitN(rule.Host, ".", 2)
-		// newHost := strings.Replace(hostArr[0], oldSubdomain, Subdomain, 1) + "." + hostArr[1]
+		newHost := strings.Replace(hostArr[0], oldSubdomain, Subdomain, 1) + "." + hostArr[1]
 		internal.Logger.Sugar().Debugf("hostArr[0]: %v, oldSubdomain: %v, Subdomain: %v", hostArr[0], oldSubdomain, Subdomain)
 		if hostArr[0] != oldSubdomain {
 			internal.Logger.Sugar().Warnf(" hostArr[0] != oldSubdomain,  hostArr[0]: %v, oldSubdomain: %v, Subdomain: %v", hostArr[0], oldSubdomain, Subdomain)
 		}
-		newHost := Subdomain + "." + hostArr[1]
+		// newHost := Subdomain + "." + hostArr[1]
 		ingress.Spec.Rules[i].Host = newHost
 		internal.Logger.Sugar().Debugf("newHost: %v", newHost)
 	}
