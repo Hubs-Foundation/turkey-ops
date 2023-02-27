7b160082132e6412e34532197b87a353fd4349a4
tanfarming <tanfarming@hotmail.com>
2023-02-27 17:20:48 -0500

    word

--- deps/ytdl/api_flask/main.py
+++ deps/ytdl/api_flask/main.py
@@ -192,13 +192,16 @@ def cloudrun_rollout_restart():
     knativeJsonStr='''
     {{"apiVersion": "serving.knative.dev/v1",
     "kind": "Service",
-    "metadata": {{"name": "{svcName}","namespace": "{projectId}"}},
+    "metadata": {{
+        "name": "{svcName}",
+        "namespace": "{projectId}"}},
+        "annotations": {{
+            "run.googleapis.com/ingress": "internal"}}
     "spec": {{
         "template": {{
         "metadata": {{
             "name": "{revisionName}",
             "annotations": {{
-                "run.googleapis.com/ingress": "internal",
                 "run.googleapis.com/vpc-access-egress": "private-ranges-only",
                 "autoscaling.knative.dev/minScale": "{minScale}",
                 "autoscaling.knative.dev/maxScale": "{maxScale}",
