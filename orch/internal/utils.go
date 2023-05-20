ea3b37d026cc5fea1d5e72cffe2be4d098768878
tanfarming <tanfarming@hotmail.com>
2023-05-20 14:15:37 -0400

    word

--- orch/internal/utils.go
+++ orch/internal/utils.go
@@ -41,6 +41,25 @@ func InitLogger() {
 	encoderCfg.FunctionKey = "f"
 	encoderCfg.MessageKey = "m"
 	// encoderCfg.FunctionKey = "f"
+	encoderCfg.LevelKey = "severity"
+	encoderCfg.EncodeLevel = func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		switch l {
+		case zapcore.DebugLevel:
+			enc.AppendString("DEBUG")
+		case zapcore.InfoLevel:
+			enc.AppendString("INFO")
+		case zapcore.WarnLevel:
+			enc.AppendString("WARNING")
+		case zapcore.ErrorLevel:
+			enc.AppendString("ERROR")
+		case zapcore.DPanicLevel:
+			enc.AppendString("CRITICAL")
+		case zapcore.PanicLevel:
+			enc.AppendString("ALERT")
+		case zapcore.FatalLevel:
+			enc.AppendString("EMERGENCY")
+		}
+	}
 
 	// Logger = zap.New(zapcore.NewCore(zapcore.NewConsoleEncoder(encoderCfg), zapcore.Lock(os.Stdout), AtomLvl), zap.AddCaller())
 	Logger = zap.New(zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), zapcore.Lock(os.Stdout), AtomLvl), zap.AddCaller())
