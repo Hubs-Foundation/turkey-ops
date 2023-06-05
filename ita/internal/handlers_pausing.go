aaf035e16b60652ab10e1b3aba4f375128330e80
tanfarming <tanfarming@hotmail.com>
2023-06-05 13:45:41 -0400

    words

--- ita/internal/handlers_pausing.go
+++ ita/internal/handlers_pausing.go
@@ -4,5 +4,8 @@ import "net/http"
 
 var Root_Pausing = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-	http.Error(w, "this hubs' paused, click the duck to try to unpause it", http.StatusOK)
+	// http.Error(w, "this hubs' paused, click the duck to try to unpause it", http.StatusOK)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	http.ServeFile(w, r, "./_statics/pausing.html")
+
 })
