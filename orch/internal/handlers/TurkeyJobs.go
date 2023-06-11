8de46d91734e69859f39d6d08882fc21c6c78052
tanfarming <tanfarming@hotmail.com>
2023-06-11 11:14:08 -0400

    word

---
+++ orch/internal/handlers/TurkeyJobs.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"main/internal"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func HandleTurkeyJobs() {
+
+	internal.Logger.Debug("")
+
+	go func() {
+		for {
+			err := internal.Cfg.Gcps.PubSub_Pulling(
+				internal.Cfg.TurkeyJobsPubSubSubName,
+				TurkeyJobRouter,
+			)
+			internal.Logger.Sugar().Errorf("err: %v", err)
+		}
+	}()
+}
+
+var TurkeyJobRouter = func(_ context.Context, msg *pubsub.Message) {
+	internal.Logger.Sugar().Debugf("received message, msg.Data :%v\n", string(msg.Data))
+	internal.Logger.Sugar().Debugf("received message, msg.Attributes :%v\n", msg.Attributes)
+	internal.Logger.Sugar().Debugf("HC_Count: %v", internal.HC_Count)
+
+	var hcCfg HCcfg
+	err := json.Unmarshal(msg.Data, &hcCfg)
+	if err != nil {
+		internal.Logger.Error("bad hcmsg.DataCfg: " + string(msg.Data))
+	}
+	switch hcCfg.JobQueueReqMethod {
+	case "POST":
+		err = CreateHubsCloudInstance(hcCfg)
+		if err != nil {
+			internal.Logger.Sugar().Errorf("failed to CreateHubsCloudInstance, err: %v, msg.Data dump: %v", err, string(msg.Data))
+		}
+	case "PATCH":
+		_, err := UpdateHubsCloudInstance(hcCfg)
+		if err != nil {
+			internal.Logger.Sugar().Errorf("failed to PatchHubsCloudInstance, err: %v, msg.Data dump: %v", err, string(msg.Data))
+		}
+	case "DELETE":
+		err := DeleteHubsCloudInstance(hcCfg)
+		if err != nil {
+			internal.Logger.Sugar().Errorf("failed to DeleteHubsCloudInstance, err: %v, msg.Data dump: %v", err, string(msg.Data))
+		}
+
+	default:
+		internal.Logger.Error("bad hcCfg.JobQueueReqMethod: " + hcCfg.JobQueueReqMethod)
+	}
+	msg.Ack()
+}
