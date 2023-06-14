bc4f57d2425696c2b46e2cbf23fa482f5ed0dec8
tanfarming <tanfarming@hotmail.com>
2023-06-14 10:47:25 -0400

    words

--- orch/internal/redis.go
+++ orch/internal/redis.go
@@ -40,7 +40,7 @@ func NewRedisSvc() *redisSvc {
 				Logger.Error("[NewRedisSvc test] failed to LPUSH _testkey: " + err.Error())
 			}
 		}()
-		val, err := r.Conn().Do("BLPop", "_testkey")
+		val, err := r.Conn().Do("BLPop", "_testkey", 10*time.Second)
 		if err != nil {
 			Logger.Sugar().Errorf("[NewRedisSvc test] failed -- err:%v", err)
 		}
