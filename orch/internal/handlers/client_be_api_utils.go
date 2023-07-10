27f5d0ccc31965950d4dde6dc4d4af25b3b35683
tanfarming <tanfarming@hotmail.com>
2023-07-09 23:18:20 -0400

    word

--- orch/internal/handlers/client_be_api_utils.go
+++ orch/internal/handlers/client_be_api_utils.go
@@ -31,6 +31,7 @@ import (
 // }
 
 func DashboardDb_getHubs(t0 time.Time) (map[int64]Turkeyorch_hubs, error) {
+	internal.Logger.Sugar().Debugf("getting * since: %v", t0)
 	hubs := make(map[int64]Turkeyorch_hubs)
 	rows, err := internal.DashboardDb.Query(context.Background(), "SELECT hub_id, name, tier, subdomain, status, account_id FROM hubs")
 	if err != nil {
