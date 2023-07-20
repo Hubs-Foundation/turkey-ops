e2e5c542bb0fe68cb4a3b34b4d4c4d23179ccb03
tanfarming <tanfarming@hotmail.com>
2023-07-20 17:17:37 -0400

    word

--- ita/internal/archMigrations.go
+++ ita/internal/archMigrations.go
@@ -11,55 +11,16 @@ func ArchMigrations() error {
 
 	Logger.Debug("placeholder")
 
-	pods, _ := cfg.K8sClientSet.CoreV1().Pods(cfg.PodNS).List(context.Background(), metav1.ListOptions{})
-
-	if len(pods.Items) == 1 && strings.HasPrefix(pods.Items[0].Name, "ita-") {
+	pods, err := cfg.K8sClientSet.CoreV1().Pods(cfg.PodNS).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		Logger.Error("ArchMigrations -- failed to list pods: " + err.Error())
+		return err
+	}
+	for _, pod := range pods.Items {
+		if !strings.HasPrefix(pod.Name, "ita-") {
+			return nil
+		}
 		orchCollect()
 	}
-
-	// AppsV1().Deployments(cfg.PodNS).Get(context.Background(), "reticulum", v1.GetOptions{})
-
-	// // if pausingLable != "yes" {
-	// // 	return nil
-	// // }
-
-	// //double check
-	// ret_d, _ := cfg.K8sClientSet.AppsV1().Deployments(cfg.PodNS).Get(context.Background(), "reticulum", v1.GetOptions{})
-	// if *ret_d.Spec.Replicas != 0 {
-	// 	Logger.Error("VERY BAD -- unexpceted paused instance, pausing==yes, but ret.replicas != 0, manual investigation needed")
-	// 	return nil
-	// }
-	// Logger.Debug("ret_d.Spec.Replicas == 0")
-	// //tripple check
-	// pods, _ := cfg.K8sClientSet.CoreV1().Pods(cfg.PodNS).List(context.Background(), v1.ListOptions{})
-	// for _, pod := range pods.Items {
-	// 	Logger.Debug("pod.Name: " + pod.Name)
-	// 	if !strings.HasPrefix(pod.Name, "ita-") {
-	// 		return nil
-	// 	}
-	// }
-
-	// Logger.Info("deleting unused svcs for pausing hc instances")
-	// svcs, err := cfg.K8sClientSet.CoreV1().Services(cfg.PodNS).List(context.Background(), v1.ListOptions{})
-	// if err != nil {
-	// 	return err
-	// }
-	// for _, svc := range svcs.Items {
-	// 	if svc.Name != "ita" {
-	// 		err := cfg.K8sClientSet.CoreV1().Services(cfg.PodNS).Delete(context.Background(), svc.Name, v1.DeleteOptions{})
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 	}
-	// }
-	// //		fix pausing label
-	// pausingLable, err := Deployment_getLabel("paused")
-	// if err != nil {
-	// 	return err
-	// }
-	// if pausingLable == "" || pausingLable == "yes" {
-	// 	t_str := time.Now().Format("060102")
-	// 	Deployment_setLabel("paused", t_str) //time.Parse("060102", t_str)
-	// }
 	return nil
 }
